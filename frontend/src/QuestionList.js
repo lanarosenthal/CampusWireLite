50cafb6a49cc02dafc3b99c4cad89331b17cdc5b
Lana <lanarosenthal@gmail.com>
2021-11-21 21:28:01 -0500

    eslint import error

--- frontend/src/QuestionList.js
+++ frontend/src/QuestionList.js
@@ -3,28 +3,25 @@ import axios from 'axios';
 
 const QuestionList = function () {
     const [questionList, setQuestionList] = useState([]);
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [succeeded, setSucceeded] = useState(false);
 
-useEffect(async () => {
-    const { data: questions } = await axios.get('/api/questions'); // GET request
-    setQuestionList(questions);
-}, []);
+    useEffect(async () => {
+        const { data: questions } = await axios.get('/api/questions'); // GET request
+        setQuestionList(questions);
+    }, []);
 
 
-  return (
-    <>
-      <h1>Questions:</h1>
-      {data.map((question) => (
-        <p>
-          {' '}
-          {question.questionText}
-          {' '}
-        </p>
-      ))}
-    </>
-  );
+    return (
+        <>
+        <h1>Questions:</h1>
+        {data.map((question) => (
+            <p>
+            {' '}
+            {question.questionText}
+            {' '}
+            </p>
+        ))}
+        </>
+    );
 };
 
 export default QuestionList;
